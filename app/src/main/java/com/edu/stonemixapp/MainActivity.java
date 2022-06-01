cf13d2fe0803e9b10b7d19000abe5a67eedb93ba
Miguel Angel Quispe Dupuy <82621486+mquisped23@users.noreply.github.com>
2022-05-31 20:11:43 -0500

    colocacion del splash screen, modificar la imagen  a mas pequeña

--- app/src/main/java/com/edu/stonemixapp/MainActivity.java
+++ app/src/main/java/com/edu/stonemixapp/MainActivity.java
@@ -8,6 +8,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Ponemos un thread para que la pantalla de carga dure 2 segundos
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //hacemos que despues de cargar la pantalla de carga se muestre la pantalla normal
+        setTheme(R.style.Theme_StoneMixApp);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
